38874801aa865c9b37f608c87ed15e7d3b9f562d
Gerard Castell <gerard.castell@cleverea.com>
2022-06-27 00:12:27 +0200

    Add confusion matrix plots + Fix scatter 3d

--- P2.m
+++ P2.m
@@ -1,4 +1,3 @@
-clear all
 %% 1.Reading and cleanning up the data
 data = readtable('agaricus-lepiota.txt','ReadVariableNames',false);
 data(:,12)=[];
@@ -15,8 +14,6 @@ data_features_n=reshape(data_feature_vec, size(data_features));
 
 [~,~, data_label_vec]=unique(data_label_c);
 data_label_n=reshape(data_label_vec, size(data_label));
-%data_label_s=data_label_n-1;
-%data_label_s(data_label_s==0)=2;
 
 %% Forming a graph
 
@@ -39,7 +36,10 @@ k=2;
 idx=kmeans(veps(:,1:2),k);
 
 %% 5. Performance evaluation & 6.Visual inspection
+confchart = figure;
+confusionchart(data_label_n, idx)
 C=confusionmat(data_label_n, idx);
+confusionchart(C);
 figure(2)
 h=gscatter(veps(:,1), veps(:,2), idx);
 title('Scatter plot. Normalized Laplacian. 2 clusters')
@@ -48,13 +48,23 @@ figure(3)
 h_r=gscatter(veps(:,1), veps(:,2), data_label_n);
 title('Sparcification using the real labels')
 
-%We repeat it with a different number of clusters
+%% We repeat it with a different number of clusters
 k=3;
 idx3=kmeans(veps(:,1:3),k);
 C3=confusionmat(data_label_n, idx3);
+confusionchart(C3);
 figure(4)
-h3=scatter3(veps(:,1), veps(:,2), veps(:,3),idx3);
-title('Scatter plot. Normalized Laplacian. 3 clusters')
+formats = [".r", ".b", ".g"];
+
+for i = 1:3
+    points = veps(idx3 == i, 1:3);
+    scatter3(points(:, 1), points(:, 2), points(:, 3), formats(i), 'DisplayName', "Cluster " + i);
+    hold on
+end
+
+legend('Location', 'Best')
+title 'Scatter plot. Normalized Laplacian. 3 clusters'
+hold off
 
 %% 7. Alternative embeddings and graoh sparcification
 
@@ -76,3 +86,4 @@ idx_rw=kmeans(veps_rw(:,1:2),k);
 figure(6)
 h_rw=gscatter(veps_rw(:,1), veps_rw(:,2), idx_rw);
 title('Scatter plot. Random-walk Laplacian. 2 clusters')
+
