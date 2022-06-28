ddef23a3369351d8f35bcf68fcc3642d82b2cbf5
CarlaLazaro <carlalazarotrilles@gmail.com>
2022-06-28 14:57:40 +0200

    Link prediction

---
+++ Link prediction/Lawyers.m
@@ -0,0 +1,16 @@
+clear all
+%% Reading and cleaning up the data
+data = readtable('ELwork36.dat','ReadVariableNames',false);
+data_m=table2array(data);
+G=graph(data_m);
+plot(G)
+
+%% Common Neighbors
+n=size(data_m,1);
+X=ones(n,n);
+X=triu(X);
+X=X-diag(diag(X));
+[row,col] = find(X);
+rc=[row, col];
+
+
